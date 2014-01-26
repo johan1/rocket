d0963cabffa4aa6b8d7da07e2bded786e16cfa57
Johan Viktorsson <johan.viktorsson.dev@gmail.com>
2014-01-26 18:10:52 +0100

    Added some stl convenience functions
    I'm getting tired of the stl default style. E.g. I rather type
    erase(C,v) instead of C.erase(remove(C.begin(), C.end(), v), C.end())

---
+++ src/common/util/StlConvenience.h
@@ -0,0 +1,26 @@
+#ifndef _STL_CONVENIENCE_H_
+#define _STL_CONVENIENCE_H_
+
+#include <algorithm>
+#include <vector>
+
+namespace rocket { namespace util {
+
+template <typename Value>
+void erase(std::vector<Value> &vec, Value const& value) {
+	vec.erase(std::remove(vec.begin(), vec.end(), value), vec.end());
+}
+
+template <typename T, typename UnaryPredicate>
+void erase_if(std::vector<T> &vec, UnaryPredicate p) {
+	vec.erase(std::remove_if(vec.begin(), vec.end(), p), vec.end());
+}
+
+template <typename Container, typename Value>
+bool contains(Container const & c, Value const& v) {
+	return std::find(std::begin(c), std::end(c), v) != std::end(c);
+}
+
+}}
+
+#endif
